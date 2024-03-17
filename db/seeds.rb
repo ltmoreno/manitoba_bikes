aebac41c2706eb484e78fa3aa28f96c33d6563dd
Lawrence <ltmoreno@outlook.com>
2024-03-16 19:09:45 -0500

    Implemented Active Admin Dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Delete tables before seeding
+AdminUser.delete_all
+
+AdminUser.create!(email: 'admin@manitobabikes.com', password: 'opAu66m6qEJCo9sJW94s', password_confirmation: 'opAu66m6qEJCo9sJW94s') if Rails.env.development?
