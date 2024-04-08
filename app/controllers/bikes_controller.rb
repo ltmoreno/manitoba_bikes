d94879c3d5ec4f3d17d49865687c511ce35c2c58
Lawrence <ltmoreno@outlook.com>
2024-04-08 02:12:24 -0500

    2.3 View the details of any of the available products on their own product page.

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,9 +1,14 @@
 class BikesController < ApplicationController
+  include BikesHelper
+
   def index
     @bikes = Bike.all
   end
 
   def show
     @bike = Bike.find_by(id: params[:id])
+    @category = @bike.category
+    @brand = @bike.brand
+    @stocks = check_bike_stock(@bike)
   end
 end
