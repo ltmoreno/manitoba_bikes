427675b646d6773a5be4ace7054e040f458d8fe3
Lawrence <ltmoreno@outlook.com>
2024-04-08 05:06:07 -0500

    2.6 Search through the available products using a keyword search by category.

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -11,4 +11,13 @@ class BikesController < ApplicationController
     @brand = @bike.brand
     @stocks = check_bike_stock(@bike)
   end
+
+  def filter
+    @category = Category.where(name: params[:category]).first
+    @search_term = params[:search]
+
+    @bikes = @category.present? ?  @bikes.where(category: @category) : Bike.all
+    #@bikes = @bikes.where(category: @category) if @category.present?
+    @bikes = @bikes.where('model LIKE ? OR description like ?', "%#{@search_term}%", "%#{@search_term}%") if @search_term.present?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :bikes, only: [:index, :show]
+  get '/filter', to: 'bikes#filter', as: 'bike_filter'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
