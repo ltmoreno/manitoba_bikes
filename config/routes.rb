859be768d4781075fa220f37c3a202730f08a644
Lawrence <ltmoreno@outlook.com>
2024-04-08 10:50:59 -0500

    2.2 Navigate through the available products by category.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :bikes, only: [:index, :show]
   get '/filter', to: 'bikes#filter', as: 'bike_filter'
+  resources :categories, only: [:show]
+
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
